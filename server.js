f947c600d1b302a015a46e09536f9ab46a45f899
duyetvn <duyetvn@viettel.com.vn>
2024-04-05 23:18:14 +0700

    init server, client, webhook, socketio

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = 3000;
+
+const PRIVATE_KEY = 'KDFJHBVCIPUASERHBN123';
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors());
+
+
+app.post('/test-webhook', (req, res) => {
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>');
+  console.log(req.body);
+  console.log('<<<<<<<<<<<<<<<<<<<<<<<<');
+
+  io.emit('data', req.body);
+
+  res.status(200).json({ message: 'OK', success: 1 });
+});
+
+// GET all items
+app.get('/items', (req, res) => {
+  io.emit('data', { test: 123 });
+  res.json([
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ]);
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on('error', (error) => {
+    console.error('Socket error:', error);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
